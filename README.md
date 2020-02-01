1306a0584c0740bbb2de22387387e805a1b16c78
Ian Walton <ian@iwalton.com>
2020-02-01 00:20:46 -0500

    Implement bindings and an event queue.

--- README.md
+++ README.md
@@ -7,3 +7,51 @@ directly communicating with MPV via the C API the performance will be worse.
 
 The win32_named_pipe module is from [KomodoEdit](https://github.com/Komodo/KomodoEdit) and is available
 under the terms of the Mozilla Public License.
+
+Please note that this only implements the subset of `python-mpv` that is used by `plex-mpv-shim` and
+`jellyfin-mpv-shim`. Other functionality has not been implemented.
+
+## Installation
+
+```python
+sudo pip3 install python-mpv-jsonrpc
+```
+
+## Basic usage
+
+Create an instance of MPV. You can use an already running MPV or have the library start a
+managed copy of MPV. Command arguments can be specified when initializing MPV if you are
+starting a managed copy of MPV.
+
+```python
+from python_mpv_jsonrpc import MPV
+
+# Uses MPV that is in the PATH.
+mpv = MPV()
+
+# Use MPV that is running and connected to /tmp/mpv-socket.
+mpv = MPV(start_mpv=False, ipc_socket="/tmp/mpv-socket")
+
+# Uses MPV that is found at /path/to/mpv.
+mpv = MPV(mpv_location="/path/to/mpv")
+
+# After you have an MPV, you can read and set (if applicable) properties.
+mpv.volume # 100.0 by default
+mpv.volume = 20 
+
+# You can also send commands.
+mpv.command_name(arg1, arg2)
+
+# Bind to key press events with a decorator
+@mpv.on_key_press("space")
+def space_handler():
+    pass
+
+# You can also observe and wait for properties.
+@mpv.property_observer("eof-reached")
+def handle_eof(name, value):
+    pass
+
+# Or simply wait for the value to change once.
+mpv.wait_for_property("duration")
+```
--- python_mpv_jsonipc/__init__.py
+++ python_mpv_jsonipc/__init__.py
@@ -5,6 +5,8 @@ import os
 import time
 import subprocess
 import random
+import traceback
+import queue
 
 if os.name == "nt":
     from .win32_named_pipe import Win32Pipe
@@ -50,7 +52,7 @@ class WindowsSocket(threading.Thread):
                 for item in data.split(b'\n'):
                     if item == '':
                         continue
-                    json_data = json.loads(data)
+                    json_data = json.loads(item)
                     self.callback(json_data)
                 data = b''
         except BrokenPipeError:
@@ -90,9 +92,9 @@ class UnixSocket(threading.Thread):
 
             data = data.decode('utf-8', 'ignore').encode('utf-8')
             for item in data.split(b'\n'):
-                if item == '':
+                if item == b'':
                     continue
-                json_data = json.loads(data)
+                json_data = json.loads(item)
                 self.callback(json_data)
             data = b''
 
@@ -104,14 +106,21 @@ class MPVProcess:
             else:
                 mpv_location = "mpv"
         
+        ipc_socket_name = ipc_socket
         if os.name == 'nt':
             ipc_socket = "\\\\.\\pipe\\" + ipc_socket
 
         if os.path.exists(ipc_socket):
             os.remove(ipc_socket)
 
+        self.ipc_socket = ipc_socket
         args = [mpv_location]
-        args.extend("--{0}={1}".format(*v) for v in kwargs.items())        
+        self._set_default(kwargs, "idle", True)
+        self._set_default(kwargs, "input_ipc_server", ipc_socket_name)
+        self._set_default(kwargs, "input_terminal", False)
+        self._set_default(kwargs, "terminal", False)
+        args.extend("--{0}={1}".format(v[0].replace("_", "-"), self._mpv_fmt(v[1]))
+                    for v in kwargs.items())
         self.process = subprocess.Popen(args)
         for _ in range(20):
             time.sleep(0.1)
@@ -122,8 +131,21 @@ class MPVProcess:
         if not os.path.exists(ipc_socket) or self.process.returncode is not None:
             raise MPVError("MPV not started.")
 
+    def _set_default(self, prop_dict, key, value):
+        if key not in prop_dict:
+            prop_dict[key] = value
+
+    def _mpv_fmt(self, data):
+        if data == True:
+            return "yes"
+        elif data == False:
+            return "no"
+        else:
+            return data
+
     def stop(self):
         self.process.terminate()
+        os.remove(self.ipc_socket)
 
 class MPVInter:
     def __init__(self, ipc_socket, callback=None):
@@ -180,14 +202,41 @@ class MPVInter:
                     return None
                 raise MPVError(data["error"])
             else:
-                return data["data"]
+                return data.get("data")
         else:
             raise TimeoutError("No response from MPV.")
+
+class EventHandler(threading.Thread):
+    def __init__(self):
+        self.queue = queue.Queue()
+        threading.Thread.__init__(self)
+    
+    def put_task(self, func, *args):
+        self.queue.put((func, args))
+
+    def stop(self):
+        self.queue.put("quit")
+        self.join()
+
+    def run(self):
+        while True:
+            event = self.queue.get()
+            if event == "quit":
+                break
+            try:
+                event[0](*event[1])
+            except:
+                traceback.print_exc()
 
 class MPV:
     def __init__(self, start_mpv=True, ipc_socket=None, mpv_location=None, **kwargs):
         self.properties = {}
+        self.event_bindings = {}
+        self.key_bindings = {}
+        self.property_bindings = {}
         self.mpv_process = None
+        self.event_handler = EventHandler()
+        self.event_handler.start()
         if ipc_socket is None:
             rand_file = "mpv{0}".format(random.randint(0, 2**48))
             if os.name == "nt":
@@ -201,15 +250,102 @@ class MPV:
         self.mpv_inter = MPVInter(ipc_socket, self._callback)
         self.properties = set(x.replace("-", "_") for x in self.command("get_property", "property-list"))
         for command in self.command("get_property", "command-list"):
-            def wrapper(*args):
-                self.command(command, *args)
-            object.__setattr__(self, command["name"].replace("-", "_"), wrapper)
+            object.__setattr__(self, command["name"].replace("-", "_"), self._get_wrapper(command["name"]))
 
         self._dir = list(self.properties)
         self._dir.extend(object.__dir__(self))
 
+        self.observer_id = 0
+        self.observer_lock = threading.Lock()
+        self.keybind_id = 0
+        self.keybind_lock = threading.Lock()
+        
+        @self.on_event("property-change")
+        def event_handler(data):
+            if data.get("id") in self.property_bindings:
+                self.event_handler.put_task(self.property_bindings[data["id"]], data["name"], data["data"])
+
+        @self.on_event("client-message")
+        def client_message_handler(data):
+            args = data["args"]
+            if len(args) == 2 and args[0] == "custom-bind":
+                self.event_handler.put_task(self.key_bindings[args[1]])
+
+    def bind_event(self, name, callback):
+        if name not in self.event_bindings:
+            self.event_bindings[name] = set()
+        self.event_bindings[name].add(callback)
+
+    def on_event(self, name):
+        def wrapper(func):
+            self.bind_event(name, func)
+            return func
+        return wrapper
+
+    def on_key_press(self, name):
+        def wrapper(func):
+            self.bind_key_press(name, func)
+            return func
+        return wrapper
+
+    def bind_key_press(self, name, callback):
+        self.keybind_lock.acquire()
+        keybind_id = self.keybind_id
+        self.keybind_id += 1
+        self.keybind_lock.release()
+
+        self.key_bindings["bind{0}".format(keybind_id)] = callback
+        self.keybind(name, "script-message custom-bind bind{0}".format(keybind_id))
+
+    def bind_property_observer(self, name, callback):
+        self.observer_lock.acquire()
+        observer_id = self.observer_id
+        self.observer_id += 1
+        self.observer_lock.release()
+
+        self.property_bindings[observer_id] = callback
+        self.command("observe_property", observer_id, name)
+        return observer_id
+
+    def unbind_property_observer(self, observer_id):
+        self.command("unobserve_property", observer_id)
+        del self.property_bindings[observer_id]
+
+    def property_observer(self, name):
+        def wrapper(func):
+            self.bind_property_observer(name, func)
+            return func
+        return wrapper
+    
+    def wait_for_property(self, name):
+        event = threading.Event()
+        def handler(*_):
+            event.set()
+        observer_id = self.bind_property_observer(name, handler)
+        event.wait()
+        self.unbind_property_observer(observer_id)
+
+    def _get_wrapper(self, name):
+        def wrapper(*args):
+            return self.command(name, *args)
+        return wrapper
+
     def _callback(self, event, data):
-        pass
+        if event in self.event_bindings:
+            for callback in self.event_bindings[event]:
+                self.event_handler.put_task(callback, data)
+
+    def play(self, url):
+        self.loadfile(url)
+
+    def __del__(self):
+        self.terminate()
+
+    def terminate(self):
+        if self.mpv_process:
+            self.mpv_process.stop()
+        self.mpv_inter.stop()
+        self.event_handler.stop()
 
     def command(self, command, *args):
         return self.mpv_inter.command(command, *args)
@@ -223,6 +359,16 @@ class MPV:
         if name not in {"properties", "command"} and name in self.properties:
             return self.command("set_property", name.replace("_", "-"), value)
         return object.__setattr__(self, name, value)
+
+    def __hasattr__(self, name):
+        if object.__hasattr__(self, name):
+            return True
+        else:
+            try:
+                getattr(self, name)
+                return True
+            except MPVError:
+                return False
 
     def __dir__(self):
         return self._dir
